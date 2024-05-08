13d4132defd4c77ac77a192e8538d48085fdc838
Gabor Szabo1 <gabor_szabo1@epam.com>
2024-05-08 15:26:56 +0200

    load the example with content and create a footer

--- src/scripts/loadJsContent.js
+++ src/scripts/loadJsContent.js
@@ -5,19 +5,25 @@ const jsfiles = await glob(`./src/content/docs/examples/*/*.js`);
 
 export async function loadFileContent(slug) {
   const lastPart = slug?.substring(slug.lastIndexOf("/") + 1);
+
+  const option = slug.includes("tools");
+
   const regex = new RegExp(`^examples\/([^/]+)\/${lastPart}$`);
 
   const fileContents = await Promise.all(
     jsfiles
       .filter(
         (fileName) =>
-          fileName.toLowerCase().includes(lastPart) || fileName.match(regex),
+          fileName.toLowerCase().includes(lastPart) || fileName.match(regex)
       )
       .map(async (file) => {
         const content = await readFile(file, { encoding: "utf8" });
-        const path = file.match(/examples\\([^\\]+)\\/)[1];
+        const path = option
+          ? file.match(/[^\\]+$/)[0]
+          : file.match(/examples\\([^\\]+)\\/)[1];
+
         return { path, content };
-      }),
+      })
   );
   return fileContents;
 }
